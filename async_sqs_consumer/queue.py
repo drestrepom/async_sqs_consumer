7930ba084ab1ba562d86539429acd59eda1e6268
Diego Restrepo <drestrepo@fluidattacks.com>
2022-10-06 19:03:28 -0500

    feat(back): retry aws operations
    - add queue operations

--- async_sqs_consumer/queue.py
+++ async_sqs_consumer/queue.py
@@ -1,14 +1,46 @@
+from aiohttp.client_exceptions import (
+    ClientConnectorError,
+    ServerDisconnectedError,
+)
 from async_sqs_consumer.resources import (
     get_sqs_client,
+)
+from async_sqs_consumer.utils.retry import (
+    retry,
+)
+from asyncio import (
+    TimeoutError,
+)
+from botocore.exceptions import (
+    ClientError,
 )
 from typing import (
     Any,
 )
 
+NETWORK_ERRORS = (
+    ServerDisconnectedError,
+    ClientConnectorError,
+    ClientError,
+    TimeoutError,
+)
 
+
+@retry(exceptions=NETWORK_ERRORS, tries=3, delay=0.2)
 async def get_queue_messages(queue_url: str) -> list[dict[str, Any]]:
     client = await get_sqs_client()
     response = await client.receive_message(
-        QueueUrl=queue_url, WaitTimeSeconds=1
+        QueueUrl=queue_url,
+        MaxNumberOfMessages=10,
+        VisibilityTimeout=600,
     )
     return response.get("Messages", [])
+
+
+@retry(exceptions=NETWORK_ERRORS, tries=3, delay=0.2)
+async def delete_messages(queue_url: str, receipt_handle: dict[str, Any]) -> None:
+    client = await get_sqs_client()
+    await client.delete_message(
+        ReceiptHandle=receipt_handle,
+        QueueUrl=queue_url,
+    )
